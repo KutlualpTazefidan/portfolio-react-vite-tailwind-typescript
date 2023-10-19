584f717386465704b5199c4c82e176bfb52c956e
Kutlualp Tazefidan <=>
2023-10-19 22:12:31 +0200

    Return to the fixed version of the sidebar

--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -38,21 +38,21 @@ export const SideNavbar = () => {
           ''
         )
       }
-        <div className='md:block hidden fixed top-[25%] z-[1]'>
-          <div className='flex flex-col pl-5'>
-            <a href='#main' className='rounded-full shadow-lg bg-black shadow-gray-400 m-2 p-4 cursor-pointer backdrop-filter backdrop-blur-lg bg-opacity-80 hover:scale-105 ease in duration-200 hover:bg-opacity-100'>
+        <div className='fixed left-0 md:block hidden top-[25%] z-[1]'>
+          <div className='flex flex-col'>
+            <a href='#main' className='rounded-full shadow-lg bg-black shadow-gray-400 mb-2 mr-4 ml-7 p-4 cursor-pointer backdrop-filter backdrop-blur-lg bg-opacity-80 hover:scale-105 ease in duration-200 hover:bg-opacity-100'>
               <AiOutlineHome size={20}  color={'white'} />
             </a>
-            <a href='#work' className='rounded-full shadow-lg bg-black shadow-gray-400 m-2 p-4 cursor-pointer backdrop-filter backdrop-blur-lg bg-opacity-80 hover:scale-105 ease in duration-200 hover:bg-opacity-100'>
+            <a href='#work' className='rounded-full shadow-lg bg-black shadow-gray-400 mb-2 mr-4 ml-7 p-4 cursor-pointer backdrop-filter backdrop-blur-lg bg-opacity-80 hover:scale-105 ease in duration-200 hover:bg-opacity-100'>
               <AiOutlineAppstore size={20} color={'white'}/>
             </a>
-            <a href='#projects' className='rounded-full shadow-lg bg-black shadow-gray-400 m-2 p-4 cursor-pointer backdrop-filter backdrop-blur-lg bg-opacity-80 hover:scale-105 ease in duration-200 hover:bg-opacity-100'>
+            <a href='#projects' className='rounded-full shadow-lg bg-black shadow-gray-400 mb-2 mr-4 ml-7 p-4 cursor-pointer backdrop-filter backdrop-blur-lg bg-opacity-80 hover:scale-105 ease in duration-200 hover:bg-opacity-100'>
               <AiOutlineProject size={20} color={'white'} />
             </a>
-            <a href='#main' className='rounded-full shadow-lg bg-black shadow-gray-400 m-2 p-4 cursor-pointer backdrop-filter backdrop-blur-lg bg-opacity-80 hover:scale-105 ease in duration-200 hover:bg-opacity-100'>
+            <a href='#main' className='rounded-full shadow-lg bg-black shadow-gray-400 mb-2 mr-4 ml-7 p-4 cursor-pointer backdrop-filter backdrop-blur-lg bg-opacity-80 hover:scale-105 ease in duration-200 hover:bg-opacity-100'>
               <BsPerson size={20} color={'white'} />
             </a>
-            <a href='#contact' className='rounded-full shadow-lg bg-black shadow-gray-400 m-2 p-4 cursor-pointer backdrop-filter backdrop-blur-lg bg-opacity-80 hover:scale-105 ease in duration-200 hover:bg-opacity-100'>
+            <a href='#contact' className='rounded-full shadow-lg bg-black shadow-gray-400 mb-2 mr-4 ml-7 p-4 cursor-pointer backdrop-filter backdrop-blur-lg bg-opacity-80 hover:scale-105 ease in duration-200 hover:bg-opacity-100'>
               <AiOutlineMail size={20} color={'white'} />
             </a>
           </div>
