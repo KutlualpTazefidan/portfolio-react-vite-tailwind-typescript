680324d7936bdc05040dd376ddeaf7ccf3a7b3d9
Kutlualp Tazefidan <=>
2023-10-19 20:03:17 +0200

    Seperate background from foreground

---
+++ src/components/Background.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+    <img className='fixed w-full h-screen object-cover object-top' src="/images/apollo14capsule.jpg" alt="Apollo 14 Capsule - Captured during a trip in Orlando" />
+  )
+}
+
+export default Background
