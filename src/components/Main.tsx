61c3aca7ec695a69ab87c69ecb189cf6cb06ed88
Kutlualp Tazefidan <=>
2023-10-19 15:03:37 +0200

    Add text/note to the page, slow down typing effect

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,7 +5,7 @@ const Main = () => {
   return (
     <div id='main'>
         <img className='w-full h-screen object-cover object-left' src="/images/apollo14capsule.jpg" alt="Apollo 14 Capsule - Captured during a trip in Orlando" />
-        <div className='w-full h-screen absolute top-0 left-0 bg-white/10'>
+        <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
             <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center items-center lg:items-start '>
                 <h1 className='sm:text-5xl text-4xl font-bold text-gray-1000'>I'm Kutlualp Tazefidan</h1>
                 <h2 className='flex sm:text-2xl text-2xl pt-4 pb-4 font-medium'>I'm
@@ -13,13 +13,13 @@ const Main = () => {
                     sequence={[
                         // Same substring at the start will only be typed out once, initially
                         "a Front-End Developer",
-                        2000, // wait 1s before replacing "Mice" with "Hamsters"
+                        4000, // wait 1s before replacing "Mice" with "Hamsters"
                         'a Data Scientist',
-                        2000,
+                        4000,
                         'a Machine Learning Enthusiast',
-                        2000,
+                        4000,
                         'an Aeronautical Engineer',
-                        2000,
+                        4000,
                     ]}
                     wrapper="span"
                     speed={50}
@@ -29,11 +29,17 @@ const Main = () => {
                     />
                 </h2>
                 <div className='flex max-w-[200px]'>
-                    <a href='https://www.linkedin.com/in/kutlualp-tazefidan/'>
-                        <div className='rounded-full bg-black p-2 shadow-gray-100 hover:scale-110 ease-in duration-200'>
-                            <FaLinkedinIn className='cursor-pointer' size={20} color={'#0072b1'}/>
-                        </div>
+                    <a href='https://www.linkedin.com/in/kutlualp-tazefidan/' className='rounded-full shadow-lg bg-black shadow-gray-400 m-2 p-4 cursor-pointer backdrop-filter backdrop-blur-lg bg-opacity-80 hover:scale-105 ease in duration-200 hover:bg-opacity-100'>
+                        <FaLinkedinIn className='cursor-pointer' size={20} color={'#0072b1'}/>
                     </a>
+                </div>
+                <div className='fixed bottom-0 mb-8 rounded-lg bg-black/50 mt-16'>
+                <h2 className='flex sm:text-1xl text-1xl p-4 font-small text-white '>
+                    As of today, 20.10.2023, my journey to the stars begins.<br/>
+                    Cheers to everyone who had a part in making it happen.<br/>
+                    Even if we never cross passes again, we all shall meet in eternity.<br/>
+                    My heart goes out to the ones, who'll be by my side wherever I go.<br/>
+                    Lots of love to everyone!</h2>
                 </div>
             </div>
         </div>
