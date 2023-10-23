f92fade45ae46b314d20ddd39a989237dab09d71
Kutlualp Tazefidan <=>
2023-10-23 11:40:24 +0200

    Create overlay between intro and webpage

---
+++ src/components/Introduction.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from './Introduction.module.css'
+
+const Introduction = () => {
+  return (
+    <div className={styles.introductionBackground}>
+        Hey, you're still here!<br/>
+        Just a quick reminder - I'm Kutlualp Tazefidan,<br/>
+        but you can totally call me Alp.<br/>
+        <br/>
+        In short, I'm your go-to for tackling the impossible. 
+        <span role="img" aria-label="smiley" className={styles.smiley}> :) </span>
+        The scrolling is over. Feel free to explore the rest.<br/>
+        </div>
+  )
+}
+
+export default Introduction
+
+// My background is in aeronautical engineering, specializing in composite materials.<br/>
+// <br/>
+// This year, I ventured into the field computer science.<br/>
+// <br/>
+// Building upon my prior experience with Python,<br/>
+// I completed two bootcamps in Fullstack Web Development and Data Science.<br/>
+// <br/>
+// Now I'm adept at developing websites with NextJs and React using Javascript.<br/>
+// <br/>
+// I can also handle the entire pipeline, from data retrieval and exploration to building machine learning models and deploying them.<br/>
+// <br/>
+// For any questions or just chat over a cup of coffee, don' just write me on linkedn<br/>
+// <br/>
+// Please don't hesitate to reach out and connect with me on LinkedIn. I'd love to hear from you!<br/>
+// <br/>
